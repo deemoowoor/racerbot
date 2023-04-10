72c4110aa2fe350b65dd16e074c3b2fba204ca3c
Andrei Sosnin <andrei.sosnin@gmail.com>
2023-04-10 22:07:31 +0000

    Improve the player's car controls to maintain speed with rotation. Refactored by extracting a few functions.

--- app.js
+++ app.js
@@ -23,6 +23,35 @@ const config = {
   }
 };
 
+// Define the track boundary coordinates
+const boundaryCoords = [
+  { x: 100, y: 50 },
+  { x: 200, y: 50 },
+  { x: 300, y: 50 },
+  { x: 400, y: 50 },
+  { x: 500, y: 50 },
+  { x: 600, y: 50 },
+  { x: 700, y: 50 },
+  { x: 750, y: 100 },
+  { x: 750, y: 200 },
+  { x: 750, y: 300 },
+  { x: 750, y: 400 },
+  { x: 750, y: 500 },
+  { x: 700, y: 550 },
+  { x: 600, y: 550 },
+  { x: 500, y: 550 },
+  { x: 400, y: 550 },
+  { x: 300, y: 550 },
+  { x: 200, y: 550 },
+  { x: 100, y: 550 },
+  { x: 50, y: 500 },
+  { x: 100, y: 550 },
+  { x: 50, y: 400 },
+  { x: 50, y: 300 },
+  { x: 50, y: 200 },
+  { x: 50, y: 100 },
+];
+
 // Create the Phaser game instance
 const game = new Phaser.Game(config);
 
@@ -36,140 +65,150 @@ function preload() {
 // Define the create function
 function create() {
 
-  this.TURN_RATE = 200;
-  this.ACCELERATION = 300;
-  this.MAX_VELOCITY = 600;
-  this.DRAG = 100;
+  this.scale = 0.2;
+  this.TURN_RATE = this.scale;
+  this.ACCELERATION = 400 * this.scale;
+  this.MAX_VELOCITY = 2000 * this.scale;
+  this.wheelAngle = 0;
+  this.wheelMaxAngle = 4;
 
   // Create a new TileSprite and set it as the background
-  this.bg = this.add.tileSprite(0, 0, this.game.config.width * 4, this.game.config.height * 4, 'background');
+  this.bg = this.add.tileSprite(0, 0,
+    this.game.config.width * 2 / this.scale,
+    this.game.config.height * 2 / this.scale, 'background').setOrigin(0.5);
+  this.bg.setScale(this.scale);
 
   // Create a group to hold the track boundary sprites
   this.boundaryGroup = this.physics.add.staticGroup();
-
-  // Define the track boundary coordinates
-  const boundaryCoords = [
-    { x: 100, y: 50 },
-    { x: 200, y: 50 },
-    { x: 300, y: 50 },
-    { x: 400, y: 50 },
-    { x: 500, y: 50 },
-    { x: 600, y: 50 },
-    { x: 700, y: 50 },
-    { x: 750, y: 100 },
-    { x: 750, y: 200 },
-    { x: 750, y: 300 },
-    { x: 750, y: 400 },
-    { x: 750, y: 500 },
-    { x: 700, y: 550 },
-    { x: 600, y: 550 },
-    { x: 500, y: 550 },
-    { x: 400, y: 550 },
-    { x: 300, y: 550 },
-    { x: 200, y: 550 },
-    { x: 100, y: 550 },
-    { x: 50, y: 500 },
-    { x: 100, y: 550 },
-    { x: 50, y: 400 },
-    { x: 50, y: 300 },
-    { x: 50, y: 200 },
-    { x: 50, y: 100 },
-  ];
 
   // Create sprites for each boundary point and add to the group
   for (let i = 0; i < boundaryCoords.length; i++) {
     const point = boundaryCoords[i];
     const sprite = this.physics.add.sprite(point.x, point.y, 'boundary');
+    sprite.setScale(this.scale);
     this.boundaryGroup.add(sprite);
   }
 
-  // Add rotation vector graphics object
-  this.rotationVector = this.add.graphics();
-  this.rotationVector.lineStyle(2, 0xffcc00);
-  this.rotationVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
-
-  // Add speed vector graphics object
-  this.speedVector = this.add.graphics();
-  this.speedVector.lineStyle(2, 0x00ff00);
-  this.speedVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
-
-  // Add acceleration vector graphics object
-  this.accelerationVector = this.add.graphics();
-  this.accelerationVector.lineStyle(2, 0x0000ff);
-  this.accelerationVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
-
-  this.dragVector = this.add.graphics();
-  this.dragVector.lineStyle(2, 0xff0000);
-  this.dragVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
-
   this.player = this.physics.add.sprite(400, 450, 'player');
   this.player.setCollideWorldBounds(true);
+  this.player.setScale(0.2);
   this.player.angle = 90;
+  this.player.body.maxVelocity.set(this.MAX_VELOCITY);
 
-  this.speedText = this.add.text(590, 10, '', { font: '14px Lucida Console', color: '#333333' }).setOrigin(0);
-  this.rotationText = this.add.text(590, 25, '', { font: '14px Lucida Console', color: '#333333' }).setOrigin(0);
+  addVectorGraphics(this);
+  const textStyle = {
+    font: 'bold 15px sans-serif',
+    stroke: '#333',
+    strokeThickness: 3,
+    color: '#FFFFFF'
+  };
+  this.speedText = this.add.text(590, 10, '', textStyle).setOrigin(0);
+  this.rotationText = this.add.text(590, 25, '', textStyle).setOrigin(0);
 
   // Enable collision detection between the player's car and the track boundaries
   this.physics.add.collider(this.player, this.boundaryGroup, gameOver);
 }
+
 // Define the update function
 function update() {
 
   const cursors = this.input.keyboard.createCursorKeys();
-  const currentVelocity = this.player.body.velocity.length();
-  this.player.body.useDamping = true;
+  
+  handleVelocityAndAcceleration(this, cursors);
+  handleWheelTurning(this, cursors);
+
+  handleGauges(this);
+  
+  handleVectorGraphics(this);
+}
+
+function handleVelocityAndAcceleration(scene, cursors) {
+  const currentVelocity = scene.player.body.velocity.length();
+  scene.player.body.useDamping = true;
+  scene.player.body.drag.set(0.3);
+
+  const playerRotRad = Phaser.Math.DegToRad(scene.player.body.rotation - 90);
+  
+  scene.physics.velocityFromRotation(playerRotRad,
+    currentVelocity,
+    scene.player.body.velocity);
+  
   if (cursors.up.isDown) {
-    this.physics.velocityFromAngle(this.player.body.rotation - 90, this.ACCELERATION,
-      this.player.body.acceleration);
+    scene.physics.velocityFromRotation(playerRotRad,
+      scene.ACCELERATION,
+      scene.player.body.acceleration);
   } else if (cursors.down.isDown) {
-    this.physics.velocityFromAngle(this.player.body.rotation - 90, -this.ACCELERATION,
-      this.player.body.acceleration);
+    // braking puts double the max acceleration
+    scene.physics.velocityFromRotation(playerRotRad,
+      -scene.ACCELERATION * 2,
+      scene.player.body.acceleration);
   } else if (currentVelocity > 0) {
-    this.player.setAcceleration(0);
-    this.player.body.drag.set(0.2 * Math.abs(Math.cos(this.player.body.rotation / 180 * Math.PI)));
+    scene.player.setAcceleration(0);
   } else {
-    this.player.setAcceleration(0);
-    this.player.body.drag.set(0);
+    scene.player.setAcceleration(0);
   }
 
   // Limit velocity to max
-  if (currentVelocity > this.MAX_VELOCITY) {
-    this.player.body.velocity.scale(this.MAX_VELOCITY / currentVelocity);
+  if (currentVelocity > scene.MAX_VELOCITY) {
+    scene.player.body.velocity.scale(scene.MAX_VELOCITY / currentVelocity);
   }
+}
 
+function handleWheelTurning(scene, cursors) {
   if (cursors.left.isDown) {
-    this.player.setAngularVelocity(-this.TURN_RATE);
+    scene.wheelAngle = Math.max(-scene.wheelMaxAngle, scene.wheelAngle - scene.TURN_RATE);
   } else if (cursors.right.isDown) {
-    this.player.setAngularVelocity(this.TURN_RATE);
+    scene.wheelAngle = Math.min(scene.wheelMaxAngle, scene.wheelAngle + scene.TURN_RATE);
   } else {
-    this.player.setAngularVelocity(0);
+    scene.wheelAngle = scene.wheelAngle / 2;
   }
 
-  this.speedText.setText(`Speed: ${Math.floor(this.player.body.speed)}`);
-  this.rotationText.setText(`Drag: ${this.player.body.drag.y} x ${this.player.body.drag.x}`);
+  scene.player.angle += scene.wheelAngle / 2;
+}
+
+function handleGauges(scene) {
+  scene.speedText.setText(`Speed: ${Math.floor(scene.player.body.speed)}`);
+  scene.rotationText.setText(`Rotation babee!`);
+
+}
+
+function addVectorGraphics(scene) {
+
+  // Add rotation vector graphics object
+  scene.rotationVector = scene.add.graphics();
+  scene.rotationVector.lineStyle(2, 0xffcc00);
+  scene.rotationVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
+
+  // Add speed vector graphics object
+  scene.speedVector = scene.add.graphics();
+  scene.speedVector.lineStyle(2, 0x00ff00);
+  scene.speedVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
+
+  // Add acceleration vector graphics object
+  scene.accelerationVector = scene.add.graphics();
+  scene.accelerationVector.lineStyle(2, 0x0000ff);
+  scene.accelerationVector.strokeLineShape(new Phaser.Geom.Line(0, 0, 0, -50));
+}
+
+function handleVectorGraphics(scene) {
 
   // Update rotation vector graphics
-  this.rotationVector.x = this.player.x;
-  this.rotationVector.y = this.player.y;
-  this.rotationVector.rotation = this.player.rotation;
-  this.rotationVector.scaleY = 2;
+  scene.rotationVector.x = scene.player.x;
+  scene.rotationVector.y = scene.player.y;
+  scene.rotationVector.rotation = scene.player.rotation;
+  scene.rotationVector.scaleY = 2;
 
   // Update speed vector graphics
-  this.speedVector.x = this.player.x;
-  this.speedVector.y = this.player.y;
-  this.speedVector.rotation = Math.atan2(this.player.body.velocity.y, this.player.body.velocity.x) + Math.PI / 2;
-  this.speedVector.scaleY = this.player.body.speed / 100 + 1;
+  scene.speedVector.x = scene.player.x;
+  scene.speedVector.y = scene.player.y;
+  scene.speedVector.rotation = Math.atan2(scene.player.body.velocity.y, scene.player.body.velocity.x) + Math.PI / 2;
+  scene.speedVector.scaleY = scene.player.body.speed / 100;
 
   // Update acceleration vector graphics
-  this.accelerationVector.x = this.player.x;
-  this.accelerationVector.y = this.player.y;
-  this.accelerationVector.rotation = this.player.body.acceleration === 0 ? this.player.rotation : Math.atan2(this.player.body.acceleration.y, this.player.body.acceleration.x) + Math.PI / 2;
-  this.accelerationVector.scaleY = Math.abs(this.player.body.acceleration.x) / 100 + Math.abs(this.player.body.acceleration.y) / 100;
-
-  this.dragVector.x = this.player.x;
-  this.dragVector.y = this.player.y;
-  this.dragVector.rotation = Math.atan2(this.player.body.drag.y, this.player.body.drag.x) + Math.PI / 2;
-  this.dragVector.scaleY = Math.abs(this.player.body.drag.x) / 100 + Math.abs(this.player.body.drag.y) / 100 + 2;
+  scene.accelerationVector.x = scene.player.x;
+  scene.accelerationVector.y = scene.player.y;
+  scene.accelerationVector.rotation = scene.player.body.acceleration === 0 ? scene.player.rotation : Math.atan2(scene.player.body.acceleration.y, scene.player.body.acceleration.x) + Math.PI / 2;
+  scene.accelerationVector.scaleY = Math.abs(scene.player.body.acceleration.length()) / 100;// + Math.abs(scene.player.body.acceleration.y) / 100;
 
 }
 
